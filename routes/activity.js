8f4e802703d966e840623b1828e90ea5e1d0103e
rvalladao <renato.valladao.f@gmail.com>
2023-12-27 14:07:12 -0300

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -134,7 +134,8 @@ exports.execute = function (req, res) {
               req.write(postData);
               req.end();*/
 
-              console.log(res);
+              console.log('req :' + req);
+              console.log('response :' + response);
               //res.status(200).send('Execute');
         } else {
             //console.error('decode failed');
