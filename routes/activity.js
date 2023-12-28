14341dec36c7b65a623f33279064d04e94689e4b
rvalladao <renato.valladao.f@gmail.com>
2023-12-28 11:16:15 -0300

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -70,7 +70,7 @@ exports.execute = function(req, res) {
             return res.status(401).end();
         }
 
-         console.log('buffer hex', req.body.toString('hex'));
+         console.log('req', JSON.stringify(req.body));
 
             var postURL = url.parse(decoded.url, true);
             const postData = decoded.data;
