6f4932594df562bd91c674d8022be0e29a28b273
rvalladao <renato.valladao.f@gmail.com>
2024-01-02 17:08:24 -0300

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -578,6 +578,7 @@ define([
 	
 		connection.trigger('updateActivity',configuration);
         console.log(configuration['arguments'].execute.body);
+		console.log(configuration['arguments'].execute.outArguments);
 	});
 	
 
