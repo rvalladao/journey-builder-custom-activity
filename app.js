3da8da14a32ff4d56eec3f4d59c084a1baba6df8
rvalladao <renato.valladao.f@gmail.com>
2024-01-02 15:19:19 -0300

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.raw({ type: 'application/jwt' }));
 //app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
 
 app.use(express.static(path.join(__dirname, 'public')));
 
