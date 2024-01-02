d4f57bafd608eda7389d1b03104965377ca45eef
rvalladao <renato.valladao.f@gmail.com>
2024-01-02 14:54:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.post('/logout', routes.logout);
 app.post('/journeybuilder/save/', activity.save);
 app.post('/journeybuilder/validate/', activity.validate);
 app.post('/journeybuilder/publish/', activity.publish);
-app.post('/journeybuilder/execute/', activity.execute, res);
+app.post('/journeybuilder/execute/', activity.execute);
 
 http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
