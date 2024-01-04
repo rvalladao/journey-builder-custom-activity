49396ba68255b4bd34ff08e1ab8728766750967f
rvalladao <renato.valladao.f@gmail.com>
2024-01-04 11:55:53 -0300

    fix

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,5 +1,3 @@
-const axios = require('axios').default;
-
 var AuthURL = "https://www.sfmc-postman.com/api/login"
 var AuthLogin = "https://www.sfmc-postman.com/users/login"
 var AuthRegister = "https://www.sfmc-postman.com/"
