6255c93c0a74a7889b2342994fe29b85abf5520b
rvalladao <renato.valladao.f@gmail.com>
2024-01-24 10:53:34 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 'use strict';
 
 var activity = require('./activity');
+var express = require('express');
+var bodyParser = require('body-parser');
+var errorhandler = require('errorhandler');
+var http = require('http');
+var path = require('path');
+var request = require('request');
+var routes = require('./routes');
+var activity = require('./api/activity');
+var getapi = require('./api/getapi');
+var apiproxy = require('./api/apiproxy');
+
+var app = express();
 
 exports.index = function (req, res) {
     console.log('index request!');
@@ -26,3 +38,30 @@ exports.login = function (req, res) {
 exports.logout = function (req, res) {
     req.session.token = '';
 };
+
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.raw({ type: 'application/jwt' }));
+//app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+if ('development' == app.get('env')) {
+  app.use(errorhandler());
+}
+
+app.get('/', routes.index);
+app.post('/login', routes.login);
+app.post('/logout', routes.logout);
+
+app.post('/journeybuilder/save/', activity.save);
+app.post('/journeybuilder/validate/', activity.validate);
+app.post('/journeybuilder/publish/', activity.publish);
+app.post('/journeybuilder/execute/', activity.execute);
+app.post('/getjourneyid/', getapi.getjid);
+app.post('/logtodataextension/', getapi.logToDataExtension);
+app.post('/apihandler/', apiproxy.apiHandler);
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
