377975c167b14d615cd41b0492efaaaec4a2922f
rvalladao <renato.valladao.f@gmail.com>
2024-02-16 09:35:45 -0300

    Update apiproxy.js

--- routes/apiproxy.js
+++ routes/apiproxy.js
@@ -23,6 +23,14 @@ exports.apiHandler = async (req, res) => {
             "crossDomain": true					
         };
 
+        function uuidv4() {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+            .replace(/[xy]/g, function (c) {
+                const r = Math.random() * 16 | 0, 
+                    v = c == 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+        }
 
         async function getResponse() {
             const responseaxios = await axios({method: settings.method, headers: settings.headers, url: settings.url, data: settings.payload});
