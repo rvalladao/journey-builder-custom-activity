ce5bd3ebac0dddd3b3994c012050bdf4e0a9de3b
rvalladao <renato.valladao.f@gmail.com>
2024-02-16 15:41:36 -0300

    Update activity.js

--- routes/activity.js
+++ routes/activity.js
@@ -69,7 +69,7 @@ exports.execute = async function (req, res) {
     console.log(JSON.stringify(decoded));
 
     const guid_id = uuidv4();
-    var postData = decoded.data.replace("GUID()", "teste");
+    var postData = decoded.data.replace(/GUID()/g, "teste");
 
     try {
         var postURL = url.parse(decoded.url, true);
